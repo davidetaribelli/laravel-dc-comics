0fd4fd486dc0b404f4dd3e343deb3d8244833ada
davidetaribelli <davide.taribelli@gmail.com>
2023-07-04 23:39:54 +0200

    feat: add navbar in header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,7 +5,16 @@
                 <img class="logo" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo-dc">
             </div>
             <div class="col-8 d-flex align-items-center justify-content-evenly">
-                
+                <a class="text-decoration-none text-black mx-3 d-flex align-items-center" href="#">CHARACTERS</a>
+                <a class="text-decoration-none text-black mx-3 d-flex align-items-center" href="#">MOVIES</a>
+                <a class="text-decoration-none text-black mx-3 d-flex align-items-center" href="#">TV</a>
+                <a class="text-decoration-none text-black mx-3 d-flex align-items-center" href="#">GAMES</a>
+                <a class="text-decoration-none text-black mx-3 d-flex align-items-center" href="#">COLLECTIBLES</a>
+                <a class="text-decoration-none text-black mx-3 d-flex align-items-center" href="#">VIDEOS</a>
+                <a class="text-decoration-none text-black mx-3 d-flex align-items-center" href="#">FANS</a>
+                <a class="text-decoration-none text-black mx-3 d-flex align-items-center" href="#">NEWS</a>
+                <a class="text-decoration-none text-black mx-3 d-flex align-items-center" href="#">SHOP</a>
+                <a class="text-decoration-none text-black mx-3 d-flex align-items-center" href="#">COMICS</a>
             </div>
         </div>
     </div>
